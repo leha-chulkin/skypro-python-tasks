2bf6c94b2fb7a5afa97568be71d5aff3cd390491
leha-chulkin <leha.chulkin.96@mail.ru>
2026-01-19 13:40:01 +0300

    Обновление домашней работы: удалены тесты, добавлены новые файлы

--- 06K_lesson/06K_lesson.3.py
+++ 06K_lesson/06K_lesson.3.py
@@ -1,27 +1,14 @@
 import pytest
-from typing import Any
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
-@pytest.fixture
-def driver():
-    driver = webdriver.Firefox()
-    driver.maximize_window()
-    yield driver
-    driver.quit()
-
-
-def test_shopping_process(driver: Any):
+def test_shopping_process(driver):
     wait = WebDriverWait(driver, 10)
     driver.get("https://www.saucedemo.com/")
 
     # Авторизация
-    wait.until(
-        EC.visibility_of_element_located((By.ID, 'user-name'))
-    ).send_keys('standard_user')
+    wait.until(EC.visibility_of_element_located((By.ID, 'user-name'))).send_keys('standard_user')
     driver.find_element(By.ID, 'password').send_keys('secret_sauce')
     driver.find_element(By.ID, 'login-button').click()
 
@@ -33,34 +20,23 @@ def test_shopping_process(driver: Any):
     ]
 
     for product_id in products:
-        btn = wait.until(
-            EC.element_to_be_clickable(
-                (By.ID, f'add-to-cart-{product_id}')
-            )
-        )
+        btn = wait.until(EC.element_to_be_clickable((By.ID, f'add-to-cart-{product_id}')))
         btn.click()
 
     # Перейти в корзину
-    wait.until(
-        EC.element_to_be_clickable((By.CLASS_NAME, 'shopping_cart_link'))
-    ).click()
+    wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'shopping_cart_link'))).click()
 
     # Checkout
     wait.until(EC.element_to_be_clickable((By.ID, 'checkout'))).click()
 
     # Заполнение формы
-    wait.until(
-        EC.visibility_of_element_located((By.ID, 'first-name'))
-    ).send_keys('Имя')
+    wait.until(EC.visibility_of_element_located((By.ID, 'first-name'))).send_keys('Имя')
     driver.find_element(By.ID, 'last-name').send_keys('Фамилия')
     driver.find_element(By.ID, 'postal-code').send_keys('12345')
     driver.find_element(By.ID, 'continue').click()
 
     # Проверка итоговой суммы
-    total_element = wait.until(
-        EC.visibility_of_element_located(
-            (By.CLASS_NAME, 'summary_subtotal_label')
-        )
-    )
+    total_element = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'summary_subtotal_label')))
     total_text = total_element.text
-    assert '$58.29' in total_text
+    # Ожидаемый формат: "Item total: $58.29"
+    assert total_text == 'Item total: $58.29'
